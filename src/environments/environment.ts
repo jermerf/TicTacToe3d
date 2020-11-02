31c717a4d219068493c218654518f6041d720d6b
Jermaine <jermerf@gmail.com>
2020-11-01 23:18:50 -0400

    Ok, figure out angular process.env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
-import * as process from 'process'
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: process.env.NODE_ENV == 'production'
+  production: true
 };
 
 /*
